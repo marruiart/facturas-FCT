f455067c12bb083aa38c36d5e683ddbe0baaf5be
Marina Ruiz Artacho <marruiart@gmail.com>
2024-05-06 09:43:23 +0200

    Change password toggle with the theme

--- app/src/main/java/com/marinaruiz/facturas_fct/core/utils/colors.kt
+++ app/src/main/java/com/marinaruiz/facturas_fct/core/utils/colors.kt
@@ -1,10 +1,17 @@
 package com.marinaruiz.facturas_fct.core.utils
 
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
-import androidx.annotation.ColorRes
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import com.google.android.material.color.MaterialColors
 
 
 fun getThemeColor(attr: Int, view: View): Int {
     return MaterialColors.getColor(view, attr)
 }
+
+@ColorInt
+fun Context.themeColor(@AttrRes attrRes: Int): Int =
+    TypedValue().apply { theme.resolveAttribute(attrRes, this, true) }.data
--- app/src/main/java/com/marinaruiz/facturas_fct/domain/TogglePasswordVisibilityUseCase.kt
+++ app/src/main/java/com/marinaruiz/facturas_fct/domain/TogglePasswordVisibilityUseCase.kt
@@ -3,35 +3,34 @@ package com.marinaruiz.facturas_fct.domain
 import android.graphics.drawable.Icon
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.view.View
+import android.text.method.TransformationMethod
 import android.widget.CheckBox
 import android.widget.EditText
-import androidx.core.content.ContextCompat
-import androidx.core.widget.CompoundButtonCompat.setButtonTintList
 import com.marinaruiz.facturas_fct.R
 import com.marinaruiz.facturas_fct.di.App.Companion.context
 
+
+object ToggleState {
+    val stateMap = mapOf(
+        true to mapOf(
+            "eyeIcon" to Icon.createWithResource(context, R.drawable.ic_eye_off),
+            "transformation" to HideReturnsTransformationMethod.getInstance()
+        ), false to mapOf(
+            "eyeIcon" to Icon.createWithResource(context, R.drawable.ic_eye),
+            "transformation" to PasswordTransformationMethod.getInstance()
+        )
+    )
+}
 
 class TogglePasswordVisibilityUseCase {
 
     operator fun invoke(
         toggle: CheckBox, isChecked: Boolean, vararg editTexts: EditText
     ) {
-        val eyeIcon = Icon.createWithResource(context, R.drawable.ic_eye)
-        val eyeOffIcon = Icon.createWithResource(context, R.drawable.ic_eye_off)
-        if (isChecked) {
-            toggle.setButtonIcon(eyeOffIcon)
-            val colorStateList =
-                ContextCompat.getColorStateList(context, R.color.black)
-            setButtonTintList(toggle, colorStateList)
-            editTexts.forEach { editText ->
-                editText.transformationMethod = HideReturnsTransformationMethod.getInstance()
-            }
-        } else {
-            toggle.setButtonIcon(eyeIcon)
-            editTexts.forEach { editText ->
-                editText.transformationMethod = PasswordTransformationMethod.getInstance()
-            }
+        val state = checkNotNull(ToggleState.stateMap[isChecked])
+        toggle.setButtonIcon(state["eyeIcon"] as Icon)
+        editTexts.forEach { editText ->
+            editText.transformationMethod = state["transformation"] as TransformationMethod
         }
     }
 }
